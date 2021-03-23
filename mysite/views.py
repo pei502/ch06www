d0d32a1ef35706913c369e720c1a2f28349f83ff
CJCU-CC <108b01165@mailst.cjcu.edu.tw>
2021-03-23 10:00:18 +0800

    0323

---
+++ mysite/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from datetime import datetime
+
+
+def index(request):
+    now = datetime.now()
+    return render(request, 'index.html',locals())
+
+
+
+# Create your views here.
